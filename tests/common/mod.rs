99293c0ab11b506af568264d10c80b0a649ecccd
krns06 <125727002+krns06@users.noreply.github.com>
2026-01-08 22:16:11 +0900

    refactor tests

---
+++ tests/common/mod.rs
@@ -0,0 +1,56 @@
+pub(crate) use std::{
+    fs::{self, File},
+    io::{BufReader, Read},
+    path::Path,
+};
+
+use tiny_rv32ima_sim::cpu::Cpu;
+
+pub const TEST_DIR: &str = "tests/isa/flats";
+
+pub struct RiscvTest<'a> {
+    pub filename: &'a str,
+    pub exit_address: u32,
+}
+
+fn run_test<P: AsRef<Path>>(cpu: &mut Cpu, file_path: P, exit_address: u32) {
+    let file = File::open(file_path).unwrap();
+    let mut reader = BufReader::new(file);
+
+    let mut buf = vec![0; 1024 * 1024];
+    reader.read(&mut buf).unwrap();
+
+    cpu.load_flat_program(&buf);
+    assert!(cpu.debug_run(exit_address));
+}
+
+pub fn run_tests<P: AsRef<Path>>(
+    cpu: &mut Cpu,
+    dir_path: P,
+    default_exit_address: u32,
+    excludes: Vec<RiscvTest>,
+) {
+    let dir = fs::read_dir(dir_path).unwrap();
+    for file in dir.into_iter() {
+        let file_path = file.unwrap().path();
+
+        if file_path.extension().unwrap() == "bin" {
+            let filename = file_path
+                .file_name()
+                .unwrap()
+                .to_string_lossy()
+                .into_owned();
+
+            let mut exit_address = default_exit_address;
+            for exclude in &excludes {
+                if filename == exclude.filename {
+                    exit_address = exclude.exit_address;
+                }
+            }
+
+            println!("TRY: {}", filename);
+            run_test(cpu, file_path, exit_address);
+            println!("PASS: {}", filename);
+        }
+    }
+}
--- tests/riscv-tests-ui-flats.rs
+++ tests/riscv-tests-ui-flats.rs
@@ -1,49 +1,21 @@
-use std::{
-    fs::{self, File},
-    io::{BufReader, Read},
-    path::Path,
-};
-
 use tiny_rv32ima_sim::cpu::Cpu;
 
-const TEST_DIR: &str = "tests/isa/flats";
+use crate::common::{RiscvTest, TEST_DIR, run_tests};
 
-fn run_tests<P: AsRef<Path>>(cpu: &mut Cpu, file_path: P, riscv_tests_exit_memory_address: u32) {
-    let file = File::open(file_path).unwrap();
-    let mut reader = BufReader::new(file);
-
-    let mut buf = vec![0; 1024 * 1024];
-    reader.read(&mut buf).unwrap();
-
-    cpu.load_flat_program(&buf);
-    assert!(cpu.debug_run(riscv_tests_exit_memory_address));
-}
+mod common;
 
 #[test]
 fn test_ui_flats() {
     let mut cpu = Cpu::new();
 
     let rv32ui_p_dir = format!("{}/{}", TEST_DIR, "rv32ui-p");
-
-    let dir = fs::read_dir(rv32ui_p_dir).unwrap();
-    for file in dir.into_iter() {
-        let file_path = file.unwrap().path();
-
-        if file_path.extension().unwrap() == "bin" {
-            let filename = file_path
-                .file_name()
-                .unwrap()
-                .to_string_lossy()
-                .into_owned();
-
-            println!("try: {:?}", filename);
-
-            if filename == "rv32ui-p-ld_st.bin" {
-                run_tests(&mut cpu, file_path, 0x2000);
-            } else {
-                run_tests(&mut cpu, file_path, 0x1000);
-            }
-            println!("done: {:?}", filename);
-        }
-    }
+    run_tests(
+        &mut cpu,
+        rv32ui_p_dir,
+        0x1000,
+        vec![RiscvTest {
+            filename: "rv32ui-p-ld_st.bin",
+            exit_address: 0x2000,
+        }],
+    );
 }
