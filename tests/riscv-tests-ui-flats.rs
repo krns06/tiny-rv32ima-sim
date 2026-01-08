f6184b27803ccb7b337b787f7f1ae9dc00ce9afd
krns06 <125727002+krns06@users.noreply.github.com>
2026-01-08 13:09:48 +0900

    refactor test

--- tests/riscv-tests-ui-flats.rs
+++ tests/riscv-tests-ui-flats.rs
@@ -1,28 +1,49 @@
 use std::{
-    fs::File,
+    fs::{self, File},
     io::{BufReader, Read},
+    path::{Path, PathBuf},
 };
 
 use tiny_rv32ima_sim::cpu::Cpu;
 
 const TEST_DIR: &str = "tests/isa/flats";
 
-#[test]
-fn test_ui_flats() {
-    let first = format!("{}/{}", TEST_DIR, "rv32ui-p-add.bin");
-
-    let file = File::open(&first).unwrap();
+fn run_tests<P: AsRef<Path>>(cpu: &mut Cpu, file_path: P, riscv_tests_exit_memory_address: u32) {
+    let file = File::open(file_path).unwrap();
     let mut reader = BufReader::new(file);
 
     let mut buf = vec![0; 1024 * 1024];
-
     reader.read(&mut buf).unwrap();
 
+    cpu.load_flat_program(&buf);
+    assert!(cpu.debug_run(riscv_tests_exit_memory_address));
+}
+
+#[test]
+fn test_ui_flats() {
     let mut cpu = Cpu::new();
 
-    cpu.load_flat_program(&buf);
+    let rv32ui_p_dir = format!("{}/{}", TEST_DIR, "rv32ui-p");
 
-    let res = cpu.debug_run(0x1000);
+    let dir = fs::read_dir(rv32ui_p_dir).unwrap();
+    for file in dir.into_iter() {
+        let file_path = file.unwrap().path();
 
-    println!("{}", res);
+        if file_path.extension().unwrap() == "bin" {
+            let filename = file_path
+                .file_name()
+                .unwrap()
+                .to_string_lossy()
+                .into_owned();
+
+            println!("try: {:?}", filename);
+
+            if filename == "rv32ui-p-ld_st.bin" || filename == "rv32ui-p-ma_data.bin" {
+                run_tests(&mut cpu, file_path, 0x2000);
+            } else {
+                run_tests(&mut cpu, file_path, 0x1000);
+            }
+            println!("done: {:?}", filename);
+        }
+    }
 }
