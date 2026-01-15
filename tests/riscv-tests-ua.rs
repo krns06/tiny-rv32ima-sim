9dc9f857019741ae554e1377347d33ac2fea9587
krns06 <125727002+krns06@users.noreply.github.com>
2026-01-15 23:48:54 +0900

    pass rv32ua-*

---
+++ tests/riscv-tests-ua.rs
@@ -0,0 +1,13 @@
+use tiny_rv32ima_sim::cpu::Cpu;
+
+use crate::common::{TEST_ELVES_DIR, run_elf_tests, run_tests};
+
+mod common;
+
+#[test]
+fn test_ua_flats() {
+    let mut cpu = Cpu::new();
+
+    let rv32ua_p_dir = format!("{}/{}", TEST_ELVES_DIR, "rv32ua");
+    run_elf_tests(&mut cpu, rv32ua_p_dir, 0x80000000 | 0x1000, vec![]);
+}
